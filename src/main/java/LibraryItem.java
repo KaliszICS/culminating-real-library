39b9bad63bde5257157f49c219b94c9650971c21
Jannat <jannat.mim@student.tdsb.on.ca>
2025-06-09 17:53:05 +0000

    Add LibraryItem

---
+++ src/main/java/LibraryItem.java
@@ -0,0 +1,28 @@
+class LibraryItem {
+    protected String title;
+    protected String auothor;
+    protected String description;
+    protected int yearPublish;
+    protected String genre;
+    protected double price; 
+
+    public LibraryItem (String title, String author, String description, int yearPublish, String genre, double price) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.yearPublish = yearPublish;
+        this.genre = genre;
+        this.price = price;
+    }
+
+    public displayInfo {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Description: " + description);
+        System.out.println("Year published: " + yearPublish);
+        System.out.println("Genre: " + genre);
+        System.out.println("Price: $" + price);
+
+    }
+
+}
